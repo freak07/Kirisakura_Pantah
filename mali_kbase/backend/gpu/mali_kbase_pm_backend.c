0faed0efdc2c07a4190232e90b97071e57d4227b
Jack Diver <diverj@google.com>
2022-06-10 16:16:40 +0000

    mali_kbase: WAR for unexpected SC pwrup
    If SC's are unexpectedly active when we attempt to power off the SC rail,
    abort the power off
    
    Bug: 234962632
    Signed-off-by: Jack Diver <diverj@google.com>
    Change-Id: Ifdb5375001879a38b003a73a88adfa45cba26c21

--- mali_kbase/backend/gpu/mali_kbase_pm_backend.c
+++ mali_kbase/backend/gpu/mali_kbase_pm_backend.c
@@ -1278,11 +1278,13 @@ void kbase_pm_turn_off_sc_power_rails(struct kbase_device *kbdev)
 	kbase_pm_lock(kbdev);
 	WARN_ON(!kbdev->pm.backend.gpu_powered);
 	if (!kbdev->pm.backend.sc_power_rails_off) {
+		bool abort;
 		spin_lock_irqsave(&kbdev->hwaccess_lock, flags);
-		WARN_ON(!kbdev->pm.backend.sc_pwroff_safe);
 		kbdev->pm.backend.sc_power_rails_off = true;
+		/* Work around for b/234962632 */
+		abort = WARN_ON(!kbdev->pm.backend.sc_pwroff_safe);
 		spin_unlock_irqrestore(&kbdev->hwaccess_lock, flags);
-		if (kbdev->pm.backend.callback_power_off_sc_rails)
+		if (kbdev->pm.backend.callback_power_off_sc_rails && !abort)
 			kbdev->pm.backend.callback_power_off_sc_rails(kbdev);
 	}
 	kbase_pm_unlock(kbdev);
