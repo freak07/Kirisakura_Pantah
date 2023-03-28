1064e71aa12ceab01412619e5b29bc6745b1790a
freak07 <michalechner92@googlemail.com>
2023-03-28 19:51:55 +0200

    Revert "[PATCH v3 30/35] mm: introduce per-VMA lock statistics"
    This reverts commit 1e7a0c3bc90c4fa4426ded930e28111c5007e7d0.
    
    (cherry picked from commit 4b930980901410ad538c1fadb69e7136227c0f24)
    (cherry picked from commit 82c4611db126246cd1ed85c81e50c7ac63db818e)

--- mm/memory.c
+++ mm/memory.c
@@ -4770,7 +4770,6 @@ retry:
 	/* Check if the VMA got isolated after we found it */
 	if (vma->detached) {
 		vma_end_read(vma);
-		count_vm_vma_lock_event(VMA_LOCK_MISS);
 		/* The area was replaced with another one */
 		goto retry;
 	}
@@ -4779,7 +4778,6 @@ retry:
 	return vma;
 inval:
 	rcu_read_unlock();
-	count_vm_vma_lock_event(VMA_LOCK_ABORT);
 	return NULL;
 }
 #endif /* CONFIG_PER_VMA_LOCK */
