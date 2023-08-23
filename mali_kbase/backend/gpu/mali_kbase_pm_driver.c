959f31c250e0e92c846a902d41cfc1e0fde71970
Varad Gautam <varadgautam@google.com>
2023-08-23 15:07:47 +0000

    mali_kbase: Unmask RESET_COMPLETED irq before resetting the GPU
    This reverts commit e8fa58acd3f48031ac6297e64ae996d430fa31d4.
    
    b/290409680 shows RESET_COMPLETED irq being missed in the absence of this
    patch. The original patch (pa/I2dcec3ef431) was found to not cause the
    stability problems for which it was reverted (pa/I6ee1b26063a).
    Reintroduce it.
    
    Test: Boot to home.
    ARM Upstreaming: Pending in https://code.ipdelivery.arm.com/c/GPU/mali-ddk/+/4835
    Bug: 290409680
    Change-Id: Iada51bb1d9c88dd0d3b0fdab5961c85699221b47
    Signed-off-by: Varad Gautam <varadgautam@google.com>

--- mali_kbase/backend/gpu/mali_kbase_pm_driver.c
+++ mali_kbase/backend/gpu/mali_kbase_pm_driver.c
@@ -3316,6 +3316,9 @@ static int kbase_pm_do_reset(struct kbase_device *kbdev)
 
 	KBASE_TLSTREAM_JD_GPU_SOFT_RESET(kbdev, kbdev);
 
+	/* Unmask the reset complete interrupt only */
+	kbase_reg_write(kbdev, GPU_CONTROL_REG(GPU_IRQ_MASK), RESET_COMPLETED);
+
 	if (kbdev->pm.backend.callback_soft_reset) {
 		ret = kbdev->pm.backend.callback_soft_reset(kbdev);
 		if (ret < 0)
@@ -3326,9 +3329,6 @@ static int kbase_pm_do_reset(struct kbase_device *kbdev)
 		kbase_reg_write(kbdev, GPU_CONTROL_REG(GPU_COMMAND),
 				GPU_COMMAND_SOFT_RESET);
 	}
-
-	/* Unmask the reset complete interrupt only */
-	kbase_reg_write(kbdev, GPU_CONTROL_REG(GPU_IRQ_MASK), RESET_COMPLETED);
 
 	/* Initialize a structure for tracking the status of the reset */
 	rtdata.kbdev = kbdev;
