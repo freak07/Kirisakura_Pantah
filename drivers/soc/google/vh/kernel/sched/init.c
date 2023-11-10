18190f8894411786f699ead1baf6d397f3e8f7b3
freak07 <michalechner92@googlemail.com>
2023-11-10 10:15:58 +0100

    Revert "vh: sched: add sched_newidle_balance"
    This reverts commit 8957c99fa7da96cb7bd542c283d0324f070e1ccc.
    
    (cherry picked from commit 8bbd19481fdc456871fa18723d46e31155b90cf0)
    (cherry picked from commit 1e68d7f1883e5508a576f7de9757531f600f98a1)
    (cherry picked from commit f7fa0f6869d4ca647c790f26aeebc0d315eabdae)
    (cherry picked from commit a1409ab8e02026553d3aedfd46884f8253ddb5aa)

--- drivers/soc/google/vh/kernel/sched/init.c
+++ drivers/soc/google/vh/kernel/sched/init.c
@@ -93,8 +93,6 @@ extern void rvh_remove_entity_load_avg_pixel_mod(void *data, struct cfs_rq *cfs_
 						 struct sched_entity *se);
 extern void rvh_update_blocked_fair_pixel_mod(void *data, struct rq *rq);
 #endif
-void sched_newidle_balance_pixel_mod(void *data, struct rq *this_rq, struct rq_flags *rf,
-		int *pulled_task, int *done);
 extern void rvh_set_user_nice_pixel_mod(void *data, struct task_struct *p, long *nice,
 					bool *allowed);
 extern void rvh_setscheduler_pixel_mod(void *data, struct task_struct *p);
@@ -285,11 +283,6 @@ static int vh_sched_init(void)
 
 	ret = register_trace_android_rvh_check_preempt_wakeup(
 		rvh_check_preempt_wakeup_pixel_mod, NULL);
-	if (ret)
-		return ret;
-
-	ret = register_trace_android_rvh_sched_newidle_balance(
-		sched_newidle_balance_pixel_mod, NULL);
 	if (ret)
 		return ret;
 
