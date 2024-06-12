91425994a94150e7ca881c6efa136f1122fafc80
freak07 <michalechner92@googlemail.com>
2024-06-12 07:53:40 +0200

    kernel: fork: fix upstream code

--- kernel/fork.c
+++ kernel/fork.c
@@ -562,9 +562,9 @@ static __latent_entropy int dup_mmap(struct mm_struct *mm,
 	}
 #ifdef CONFIG_PER_VMA_LOCK
 	/* Disallow any page faults before calling flush_cache_dup_mm */
-	for_each_vma(old_vmi, mpnt)
+	for_each_vma(vmi, mpnt)
 		vma_start_write(mpnt);
-	vma_iter_set(&old_vmi, 0);
+	vma_iter_set(&vmi, 0);
 #endif
 	flush_cache_dup_mm(oldmm);
 	uprobe_dup_mmap(oldmm, mm);
